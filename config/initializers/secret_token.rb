143863875e5bffb4a1c78fed11267f805f88249b
Ron Arellano <ronarellano@gmail.com>
2013-03-06 16:43:48 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamViewer::Application.config.secret_token = '09479aeb40758deaa68ba2808d330379954f271cf3f72c7d83b5aa2d7030f9eb31d86c8ce4dc00d41863864a8609123afd4a0c37e2e5a25a444582b13c67548c'
